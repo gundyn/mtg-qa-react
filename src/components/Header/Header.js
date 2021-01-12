e6e790e74efda5b7c50970e41282c8e2e367eebd
gundyn <nathan.gundy@gmail.com>
2021-01-12 12:52:12 -0600

    feat: dark mode
    added a passive dark mode based on users system settings.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar className="navBar" variant="dark" expand="md">
     <Navbar.Brand href="#">
       Wizardly Questions
     </Navbar.Brand>
