73a926782b8ca09afc6f482d45cfc6f509c6d8fa
gundyn <nathan.gundy@gmail.com>
2021-01-13 13:58:01 -0600

    fix: landing page
    added content describing website

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -8,7 +8,8 @@ const LandingPage = props => (
     <div className='row'>
       <div className='col-sm-10 col-md-8 mx-auto mt-5'>
         <img src={Background} className='theBanner' alt='art work for the card Phenax, God of Deception' />
-        <p>description of website, lets see if the text stays in the center of the page woohoo!</p>
+        <h1>Welcome!</h1>
+        <p>This site is a place to ask questions and recieve answers to all of your Magic the Gathering questions. When you and your friends need an unbiased ruling on how cards interact come here to ask fellow wizards!</p>
       </div>
     </div>
   </Fragment>
